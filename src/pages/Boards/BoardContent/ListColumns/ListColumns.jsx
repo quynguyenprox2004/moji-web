39d776af7cf687e43fb683b133f3132664a264e7
Nguyen Xuan Quy <quynguyen.16092004@gmail.com>
2026-05-23 01:38:57 +0700

    finish  base board-content

---
+++ src/pages/Boards/BoardContent/ListColumns/ListColumns.jsx
@@ -0,0 +1,50 @@
+import Box from '@mui/material/Box'
+import Column from './Column/Column'
+import Button from '@mui/material/Button'
+import AddIcon from '@mui/icons-material/Add'
+
+function ListColumns() {
+  return (
+    <Box sx={{
+      backgroundColor: 'inherit',
+      width: '100%',
+      height: '100%',
+      display: 'flex',
+      overflowX: 'auto',
+      overflowY: 'hidden',
+      '&::-webkit-scrollbar-track': { m: 2 }
+    }}>
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+      <Column />
+
+      <Box sx={{
+        minWidth: '300px',
+        maxWidth: '300px',
+        mx: 2,
+        borderRadius: '6px',
+        height: 'fit-content',
+        backgroundColor: 'background.columns'
+      }}>
+        <Button
+          startIcon={<AddIcon />}
+          sx={{
+            color: 'text.primary',
+            width: '100%',
+            justifyContent: 'flex-start',
+            pl: 2,
+            py: 1
+          }}
+        >
+          Add another list
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
+export default ListColumns
